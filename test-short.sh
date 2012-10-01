0c199f74f997e9fd2db561157008ce46b63766e9
stefanocudini <stefano.cudini@gmail.com>
2012-10-01 15:21:43 +0200

    clean dir stef branch

--- test-short.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APIKEY=AIzaSyB7xuRXSoI8V5c8wD6dI8kS5ipCjLN3HQE
-#curl -XPOST -H 'Content-type: application/json' -d '{longUrl:"http://repubblica.it"}' https://www.googleapis.com/urlshortener/v1/url?key=$APIKEY
-
-curl -XPOST -H 'Content-type: application/json' -d '{longUrl:"http://repubblica.it"}' "http://localhost:8080/http_proxy/proxy/?url=https%3A//www.googleapis.com/urlshortener/v1/url%3Fkey%3D$APIKEY"
-#curl "http://localhost:8080/http_proxy/proxy/?url=https%3A//www.googleapis.com/urlshortener/v1/url%3Fkey%3D$APIKEY&longUrl%3Dhttp%3A//repubblica.it"
-
