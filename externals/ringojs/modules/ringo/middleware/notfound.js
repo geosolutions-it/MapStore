9892c2dde4a53b187031d086b04e1f03f2286952
tobia <tobia.dipisa@geo-solutions.it>
2012-11-08 13:20:57 +0100

    Fixing ringo file not found response

--- externals/ringojs/modules/ringo/middleware/notfound.js
+++ externals/ringojs/modules/ringo/middleware/notfound.js
@@ -27,8 +27,9 @@ exports.middleware = function(skin) {
             } catch (e if e.notfound) {
                 var res = Response.skin(skin, {
                     title: "Not Found",
-                    body: "<p>The requested URL <b>" + request.scriptName
-                            + request.pathInfo + "</b> was not found on the server.</p>"
+                    body: "<p>The requested URL was not found on the server.</p>"
+					//body: "<p>The requested URL <b>" + request.scriptName
+                    //        + request.pathInfo + "</b> was not found on the server.</p>"
                 });
                 res.status = 404;
                 res.contentType = 'text/html';
