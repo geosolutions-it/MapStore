c0374d1659c1d920644a6494100cf08e0219746a
r3-gis <info@r3-gis.com>
2012-12-06 11:05:59 -0500

    create a new config file

---
+++ mapcomposer/app/static/config/changeMatrix.js
@@ -0,0 +1,122 @@
+{
+   "geoStoreBase":"http://localhost:8080/geostore/rest/",
+   "proxy":"/http_proxy/proxy/?url=",
+   "defaultLanguage": "en",
+   "gsSources":{ 
+		"mapquest": {
+			"ptype": "gxp_mapquestsource"
+		}, 
+		"osm": { 
+			"ptype": "gxp_osmsource"
+		},
+		"google": {
+			"ptype": "gxp_googlesource" 
+		},
+		"bing": {
+			"ptype": "gxp_bingsource" 
+		}, 
+		"ol": { 
+			"ptype": "gxp_olsource" 
+		}
+	},
+	"map": {
+		"projection": "EPSG:900913",
+		"units": "m",
+		"center": [1250000.000000, 5370000.000000],
+		"zoom":5,
+		"maxExtent": [
+			-20037508.34, -20037508.34,
+			20037508.34, 20037508.34
+		],
+		"layers": [
+			{
+				"source": "bing",
+				"title": "Bing Aerial",
+				"name": "Aerial",
+				"group": "background"
+			}, {
+				"source": "osm",
+				"title": "Open Street Map",
+				"name": "mapnik",
+				"group": "background"
+			},{
+				"source": "mapquest",
+				"title": "MapQuest OpenStreetMap",
+				"name": "osm",
+				"group": "background"
+			},{
+				"source": "google",
+				"title": "Google Roadmap",
+				"name": "ROADMAP",
+				"group": "background"
+			},{
+				"source": "google",
+				"title": "Google Terrain",
+				"name": "TERRAIN",
+				"group": "background"
+			},{
+				"source": "google",
+				"title": "Google Hybrid",
+				"name": "HYBRID",
+				"group": "background"
+			}
+		]
+	},
+	"cswconfig": {
+		"catalogs": [
+		        {"name": "PTA", "url": "http://pta.partout.it/geoportalPTA/csw", "description": "Piattaforma Tecnologica alpina", "metaDataOptions":{"base":"http://pta.partout.it/geoportalPTA/catalog/search/resource/details.page","idParam":"uuid","idIndex":0}},
+				{"name": "Treviso", "url": "http://ows.provinciatreviso.it/geonetwork/srv/it/csw", "description": "Treviso Geonetwork"},
+				{"name": "kscNet", "url": "http://geoportal.kscnet.ru/geonetwork/srv/ru/csw", "description": "kscNet"},
+				{"name": "CSI-CGIAR", "url": "http://geonetwork.csi.cgiar.org/geonetwork/srv/en/csw", "description" : "CSI-CGIAR"},
+				{"name": "EauFrance", "url": "http://sandre.eaufrance.fr/geonetwork/srv/fr/csw", "description" : "EauFrance"},
+				{"name": "SOPAC", "url": "http://geonetwork.sopac.org/geonetwork/srv/en/csw", "description" : "SOPAC"},
+				{"name": "SADC", "url": "http://www.sadc.int/geonetwork/srv/en/csw", "description" : "SADC"},
+				{"name": "MAPAS", "url": "http://mapas.mma.gov.br/geonetwork/srv/en/csw", "description" : "MAPAS"}
+			],
+		"dcProperty": "title",
+		"initialBBox": {
+		   "minx":-13,
+		   "miny":10,
+			"maxx":-10,
+			"maxy":13
+		}, 
+		"cswVersion": "2.0.2",
+		"filterVersion": "1.1.0",
+		"start": 1,
+		"limit": 10,
+		"timeout": 60000
+	},
+	
+	"scaleOverlayUnits":{
+        "bottomOutUnits":"nmi",    
+        "bottomInUnits":"nmi",    
+        "topInUnits":"m",    
+        "topOutUnits":"km"
+    },
+
+	"customTools":[{
+            "ptype": "gxp_changematrix",
+            "actionTarget": {"target": "paneltbar", "index": 26},
+            "wpsManagerID": "wpsManager",
+            "rasterLayers": ["test1", "test2", "test3"],
+            "classes": [1, 2, 3, 4, 5, 6]
+        },{
+           "ptype": "gxp_wpsmanager",
+           "id": "wpsManager",
+           "url": "http://84.33.1.254/geoserver/wps",
+           "geostoreUrl": "http://192.168.188.130:8081/geostore/rest",
+           "geostoreUser": "admin",
+           "geostorePassword": "admin"
+        },{
+            "actions": ["->"],
+            "actionTarget": "paneltbar"
+        },{
+			"ptype": "gxp_googlegeocoder",
+			"outputConfig": {
+				"emptyText": "Google GeoCoder"
+			},
+			"outputTarget":"paneltbar",
+			"index": 26
+		}
+	]
+}
