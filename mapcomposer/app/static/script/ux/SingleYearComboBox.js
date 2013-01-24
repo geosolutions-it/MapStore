78df76774c428836d1fff4cbc4b169b05ea56b03
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2013-01-24 12:03:17 +0100

    add prototype of single year combo box component

---
+++ mapcomposer/app/static/script/ux/SingleYearComboBox.js
@@ -0,0 +1,44 @@
+/**
+ *  Copyright (C) 2007 - 2012 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @author Lorenzo Natali
+ */
+ 
+Ext.ux.SingleYearComboBox = Ext.extend(Ext.form.ComboBox,{
+	xtype: 'singleyearcombobox',
+	fieldLabel: 'Year',
+	typeAhead: true,
+	triggerAction: 'all',
+	lazyRender:false,
+	mode: 'local',
+	autoLoad:true,
+	displayField:'year',
+	valueField:'year',
+	store:new Ext.data.ArrayStore({
+		data: [
+				[2000],[2001],[2002],[2003],[2004],[2005],[2006],[2006],[2007],[2008],[2009],[2010],[2011],[2012]//TODO externalize data
+				
+		],
+		fields:[{name:'year',dataIndex:0}]
+		
+	})
+});
+
+Ext.reg( 'singleyearcombobox',Ext.ux.SingleYearComboBox);
