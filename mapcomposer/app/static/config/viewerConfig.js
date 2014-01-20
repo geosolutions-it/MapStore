ee84ee9fd5fd9a9baf38b0c3307791c92ede011c
tobia <tobia.dipisa@geo-solutions.it>
2014-01-20 12:14:03 +0100

    #205, aligned new modifications also in viewer template

--- mapcomposer/app/static/config/viewerConfig.js
+++ mapcomposer/app/static/config/viewerConfig.js
@@ -33,9 +33,8 @@
 		   "customCss": "font-weight: bold; text-shadow: 1px 0px 0px #FAFAFA, 1px 1px 0px #FAFAFA, 0px 1px 0px #FAFAFA,-1px 1px 0px #FAFAFA, -1px 0px 0px #FAFAFA, -1px -1px 0px #FAFAFA, 0px -1px 0px #FAFAFA, 1px -1px 0px #FAFAFA, 1px 4px 5px #aeaeae;color:#050505 "
 		},{
 			"ptype": "gxp_geolocationmenu",
-			"outputTarget": "paneltbar",
-			"toggleGroup": "toolGroup",
-			"index": 23
+			"actionTarget": {"target": "paneltbar", "index": 23},
+			"toggleGroup": "toolGroup"
 		}
 	]
 }
