26917705f26562be49d5a13b7fd62513c849f232
root <root@mariss.(none)>
2014-08-28 22:09:55 +0200

    - plan editor gui improvements

--- mapcomposer/app/static/externals/gxp/src/script/widgets/PlanEditorPanel.js
+++ mapcomposer/app/static/externals/gxp/src/script/widgets/PlanEditorPanel.js
@@ -5,7 +5,7 @@
  * See https://github.com/opengeo/gxp/raw/master/license.txt for the full text
  * of the license.
  */
-
+ 
 /** api: (define)
  *  module = gxp
  *  class = PlanEditorPanel
@@ -25,6 +25,11 @@ gxp.PlanEditorPanel = Ext.extend(Ext.FormPanel, {
 
     // default config
     layout: "form",
+    
+    adminUrl: null,
+    userId: null,
+    serviceId: null,
+    grid: null,
 
     /** i18n **/
     aoiText: "AOI",
@@ -45,30 +50,145 @@ gxp.PlanEditorPanel = Ext.extend(Ext.FormPanel, {
     },
 
     // sensor and sensor modes
-    sensorData: [['ESR1', 'ESR1'], ['ESR1', 'ESR2'], ['RS1', 'RS1'], ['RS2', 'RS2']],
-    sensorModeData: [['PRI', 'PRI'], ['FI', 'FI']],
-
     /** private: method[initComponent]
      *  Initialize the form panel.
      */
     initComponent: function() {
-
-        var sensorStore = new Ext.data.ArrayStore({
+        var me = this;
+        
+        var store = new Ext.data.JsonStore({
+            url: this.adminUrl + "mvc/serviceManager/extJSbrowser?action=get_servicesensorslist&folder="+this.userId+"&name="+this.serviceId,
+            autoLoad: true,
+            autoSync: true,
+            fields: ["sensor_type", "sensor_mode"]
+        });
+        var sensorStore = new Ext.data.JsonStore({
+            url: this.adminUrl + "mvc/serviceManager/extJSbrowser?action=get_sensorslist",
+            autoLoad: true,
             fields: [
-                'id',
                 'text'
-            ],
-            data: this.sensorData
+            ]
         });
 
-        var sensorModeStore = new Ext.data.ArrayStore({
+        var sensorModeStore = new Ext.data.JsonStore({
+            url: this.adminUrl + "mvc/serviceManager/extJSbrowser?action=get_sensorModeslist",
+            autoLoad: true,
             fields: [
-                'id',
                 'text'
-            ],
-            data: this.sensorModeData
+            ]
         });
-
+        
+        this.grid = new Ext.grid.GridPanel({
+            width: 450,
+            height: 110,
+            autoScroll: true,
+            frame: true,
+            layout: 'fit',
+            //title: 'Sensors',
+            store: store,
+            iconCls: 'icon-user',
+            columns: [{
+                header: 'Sensor Type',
+                width: 100,
+                sortable: true,
+                dataIndex: 'sensor_type',
+                field: {
+                    xtype: 'textfield'
+                }
+            }, {
+                header: 'Sensor Mode',
+                width: 290,
+                sortable: true,
+                dataIndex: 'sensor_mode',
+                field: {
+                    xtype: 'textfield'
+                }
+            }],
+            listeners:{
+                rowclick: function(grid, rowIndex, columnIndex, e) {
+                    var rec      = grid.store.getAt(rowIndex);
+                    me.selectedRecord = rec;
+                    Ext.getCmp(me.id + "_delete").enable();
+                },
+                scope:this
+            },
+            tbar: [{
+                xtype: 'toolbar',
+                items: [{
+                    text: 'Add',
+                    iconCls: 'icon-add',
+                    handler: function(){
+                        // empty record
+                        var sensor = {
+                            sensor_type: Ext.getCmp(me.id + "_sensor_combo").lastSelectionText,
+                            sensor_mode: Ext.getCmp(me.id + "_sensor_mode_combo").lastSelectionText
+                        };
+                        
+                        if (sensor.sensor_type && sensor.sensor_mode)
+                        {
+                            var record = new store.recordType(sensor); // create new record
+                            
+                            var recordIndex1 = store.find("sensor_type", sensor.sensor_type);
+                            var recordIndex2 = store.find("sensor_mode", sensor.sensor_mode);
+                            
+                            //
+                            // The process record is added only if missing inside the Grid
+                            //
+                            if(recordIndex1 == -1 || recordIndex2 == -1){
+                               store.addSorted(record);
+                            }
+                        }
+                    }
+                }, 
+                '-', 
+                {
+                    id: me.id + "_delete",
+                    itemId: 'delete',
+                    text: 'Delete',
+                    iconCls: 'icon-reset',
+                    disabled: true,
+                    handler: function(){
+                        var selection = me.selectedRecord;
+                        if (selection) {
+                            store.remove(selection);
+                        }
+                        Ext.getCmp(me.id + "_delete").disable();
+                    }
+                },
+                '-',
+                {
+                    xtype: "compositefield",
+                    items:[{
+                        id: me.id + "_sensor_combo",
+                        xtype: "combo",
+                        fieldLabel: me.sensorText,
+                        triggerAction: 'all',
+                        lazyRender:true,
+                        mode: 'local',
+                        valueField: 'text',
+                        displayField: 'text',
+                        store: sensorStore,
+                        name: "sensor",
+                        //allowBlank: false,
+                        width: 60
+                    },{
+                        id: me.id + "_sensor_mode_combo",
+                        xtype: "combo",
+                        fieldLabel: me.sensorModeText,
+                        triggerAction: 'all',
+                        lazyRender:true,
+                        mode: 'local',
+                        valueField: 'text',
+                        displayField: 'text',
+                        store: sensorModeStore,
+                        name: "sensorMode",
+                        //allowBlank: false,
+                        flex: 1
+                    }]
+                }]
+            }]
+        });
+        
         this.items = [{
             xtype: "fieldset",
             title: this.aoiText,
@@ -99,36 +219,13 @@ gxp.PlanEditorPanel = Ext.extend(Ext.FormPanel, {
                     width: 329
                 }]
             },{
+                
+            },{
                 xtype: "fieldset",
                 title: this.sensorText,
-                items: [{
-                    xtype: "compositefield",
-                    items:[{
-                        xtype: "combo",
-                        fieldLabel: this.sensorText,
-                        triggerAction: 'all',
-                        lazyRender:true,
-                        mode: 'local',
-                        valueField: 'id',
-                        displayField: 'text',
-                        store: sensorStore,
-                        name: "sensor",
-                        allowBlank: false,
-                        width: 60
-                    },{
-                        xtype: "combo",
-                        fieldLabel: this.sensorModeText,
-                        triggerAction: 'all',
-                        lazyRender:true,
-                        mode: 'local',
-                        valueField: 'id',
-                        displayField: 'text',
-                        store: sensorModeStore,
-                        name: "sensorMode",
-                        allowBlank: false,
-                        flex: 1
-                    }]
-                }]
+                items: [
+                    this.grid
+                ]
             }]
         }];
 
