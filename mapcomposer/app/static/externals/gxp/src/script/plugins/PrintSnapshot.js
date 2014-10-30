469deb8a26793ebd37aa59ac3879812ae476155c
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2014-10-30 12:13:39 +0100

    refactor of Print Snapshot plugin

--- mapcomposer/app/static/externals/gxp/src/script/plugins/PrintSnapshot.js
+++ mapcomposer/app/static/externals/gxp/src/script/plugins/PrintSnapshot.js
@@ -140,83 +140,11 @@ gxp.plugins.PrintSnapshot = Ext.extend(gxp.plugins.Tool, {
                 tooltip: disabled ? this.notSupportedTooltip : this.tooltip,
                 iconCls: this.iconCls,
                 disabled: disabled,
+                scope: this,
                 handler: function(btn) {
-                	var canvas = document.getElementById("printcanvas");
-					if(!canvas.getContext){
-						alert("Your browser doesn't support this tool");
-						btn.setDisabled(true);
-						return;
-					}
-                	var mapPanel = this.target.mapPanel;
-                	var map = mapPanel.map;
-                	
-                	var width = map.div.style.width;
-                	    width = width.substring(0, width.indexOf('px'));
-                	var height = map.div.style.height;
-                	    height = height.substring(0, height.indexOf('px'));
-                	
-                	var extent = map.getExtent();
-                	var layers = map.layers;
-                	
-                	var baseURL;
-                	var srsID;
-                	var unSupportedLayers = [];
-                	var supportedLayers = [];
-					var supportedStyles = [];
-                	var vectorialLayers = [];
-                	var filters = [];
-                	for (var i = 0; i < layers.length; i++) {
-                		var layer = layers[i].clone();
-                		if (layer.getVisibility()) {
-                			if (layer.url && layer instanceof OpenLayers.Layer.WMS) {
-	                			supportedLayers.push(layer.params.LAYERS);
-								supportedStyles.push(layer.params.STYLES ||'');
-	                			filters.push(layer.params.CQL_FILTER ? encodeURIComponent(layer.params.CQL_FILTER) : "INCLUDE");
-	                			
-	                			if (!baseURL) {
-	                				baseURL = layer.url;
-	                			}
-	                			
-	                			if (!srsID) {
-	                				srsID = layer.projection.projCode;
-	                			}
-	                		}
-
-                			else if (layer instanceof OpenLayers.Layer.Vector) {
-                				vectorialLayers.push(layer.clone());
-                			}
-                			
-                			else {
-                				unSupportedLayers.push(layer.clone());
-                			}
-                		}
-                	}
-                    
-                    var gsURL = baseURL + 
-                        "SERVICE=WMS" +
-                        "&LAYERS=" + supportedLayers.join(",") +
-						"&STYLES=" + supportedStyles.join(",") +
-                        "&FORMAT=" + encodeURIComponent("image/png") + 
-                        "&SRS=" + srsID +  
-                        "&VERSION=1.1.1" +
-                        "&REQUEST=GetMap" +
-                        "&BBOX=" + encodeURIComponent(extent.toBBOX())+
-                        "&WIDTH=" + width +
-                        "&HEIGHT=" + height +
-                        "&CQL_FILTER=" + filters.join(";");
-						
-                	var mHost = me.service.split("/");
-					
-                	var img = new Image();
-                	var loadMask = new Ext.LoadMask(Ext.getBody(), {msg:'Please wait...'});
-                    img.onError = function(){
-                        loadMask.hide();
-                        Ext.Msg.show({
-									 title: me.printStapshotTitle,
-									 msg: me.generatingErrorMsg,
-									 width: 300,
-									 icon: Ext.MessageBox.ERROR
-								});
+                    //regex is for IE11 so we have to use a servlet
+                    if(Ext.isIE11 === undefined){
+                        Ext.isIE11 = !!navigator.userAgent.match(/Trident.*rv[ :]*11\./);
                     }
                     if(!canvas.getContext){
 						alert("Your browser doesn't support this tool");
@@ -229,6 +157,7 @@ gxp.plugins.PrintSnapshot = Ext.extend(gxp.plugins.Tool, {
                         this.notSupportedMessage(suppLayers);
                         return;
                     }
+
                     this.startLoading();
                     var me = this;
                     html2canvas( app.mapPanel.map.div,{
@@ -274,98 +203,12 @@ gxp.plugins.PrintSnapshot = Ext.extend(gxp.plugins.Tool, {
                                    finish(c);
                                 }
 
-						// draw vectorial layers
-						if (vectorialLayers) {
-							for (var i = 0; i < map.div.getElementsByTagName('svg').length; i++) {
-								var svgTags = map.div.getElementsByTagName('svg');
-								for (var c=0; c<svgTags.length; c++) {
-					                var svgTag = svgTags[c].cloneNode(true);
-					                var div = document.createElement('div');
-					                div.appendChild(svgTag);
-					                        
-					                canvg(canvas2, div.innerHTML,{
-					                    ignoreMouse: true,
-					                    ignoreAnimation: true,
-					                    ignoreDimensions: true,
-					                    ignoreClear: true,
-					                    offsetX: 0,
-					                    offsetY: 0
-					                });
-								}
-							}
-						}
-						
-						// Print final image
-                    	canvas.getContext('2d').drawImage(canvas2, 0,0);
-		    			
-		    			// Save
-                    	var canvasData = canvas.toDataURL("image/png;base64");
-	                    
-	                    var mUrl = me.service + "UploadCanvas";
-                	    	mUrl = mHost[2] == location.host ? mUrl : proxy + mUrl;
-							
-						Ext.Ajax.request({
-						    url: mUrl,
-						    method: "POST",
-						    headers:{
-						    	  'Content-Type' : 'application/upload'
-						    },
-						    params: canvasData,
-						    scope: this,
-						    success: function(response, opts){
-                                loadMask.hide();
-								if (response.readyState == 4 && response.status == 200){
-								    if(response.responseText && response.responseText.indexOf("\"success\":false") < 0){
-								        var fname = "mapstore-snapshot.png";
-								    
-								        var mUrl = me.service + "UploadCanvas";
-								            mUrl = mHost[2] == location.host ? mUrl + "?ID=" + response.responseText + "&fn=" + fname : proxy + encodeURIComponent(mUrl + "?ID=" + response.responseText + "&fn=" + fname);
-								        me.target.safeLeaving =true;
-								        window.location.assign(mUrl);
-								        enableSaving = true;
-								    }else{
-								        // this error should go to failure
-								        Ext.Msg.show({
-								             title: me.printStapshotTitle,
-								             msg: me.generatingErrorMsg + " " + gxp.util.getResponseFailureServiceBoxMessage(response),
-								             width: 300,
-								             icon: Ext.MessageBox.ERROR
-								        });
-								    }
-								}else if (response.status != 200){
-									Ext.Msg.show({
-										 title: 'Print Snapshot',
-										 msg: this.serverErrorMsg,
-										 width: 300,
-										 icon: Ext.MessageBox.ERROR
-									});
-								}					
-						    },
-						    failure:  function(response, opts){
-                                loadMask.hide();
-								Ext.Msg.show({
-									 title: me.printStapshotTitle,
-									 msg: me.generatingErrorMsg + " " + gxp.util.getResponseFailureServiceBoxMessage(response),
-									 width: 300,
-									 icon: Ext.MessageBox.ERROR
-								});
-						    }
-						});
-                    };
-                    
-                    if (supportedLayers.length > 0){
-                    	img.src = proxy + encodeURIComponent(gsURL);
-                        loadMask.show();
-                   	}else {
-                   		Ext.Msg.show({
-			                 title: this.printStapshotTitle,
-			                 msg: this.noSupportedLayersErrorMsg,
-			                 width: 300,
-			                 icon: Ext.MessageBox.WARNING
-			            });
-                   	}
-                },
-                scope: this,
+                            }
+                    });
+                    return;
+                    //this.oldHandler(btn);
+                	},
+                
                 listeners: {
                     render: function() {
                         // wait to load until render so we can enable on success
