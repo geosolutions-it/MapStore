831e5e70d5571ef0c3da2bb841dd2edcdeb818bb
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2014-10-31 12:33:56 +0100

    Aggiornate configurazioni lose per sfruttare il flag useCapabilities: false su WMS(C)Source

--- mapcomposer/app/static/config/lose.js
+++ mapcomposer/app/static/config/lose.js
@@ -28,7 +28,8 @@
                 "TILED": true,                
                 "format": "image/png8"
             },
-			"loadingProgress": true
+			"loadingProgress": true,
+			"useCapabilities": false
         },
         "losetiled": {
             "ptype": "gxp_wmscsource",
@@ -39,7 +40,8 @@
                 "TILED": true,                
                 "format": "image/png8"
             },
-			"loadingProgress": true
+			"loadingProgress": true,
+			"useCapabilities": false
         }
     },
     "map": {
@@ -111,7 +113,8 @@
             "env":"low:100;medium:500;max:1000",
             "riskPanel":true,
             "exclusive":"SIIG",
-			
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
 			"forceOneVisible": false
         },{
             "source": "lose",
@@ -122,7 +125,8 @@
             "env":"low:100;medium:500;max:1000",
             "riskPanel":true,
             "exclusive":"SIIG",
-			
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
 			"forceOneVisible": false
         },{
             "source": "lose",
@@ -133,7 +137,8 @@
             "env":"lowsociale:100;mediumsociale:500;maxsociale:1000;lowambientale:100;mediumambientale:500;maxambientale:1000",
             "riskPanel":true,
             "exclusive":"SIIG",
-			
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
 			"forceOneVisible": false
         },
         {
@@ -141,99 +146,138 @@
             "title": "Beni culturali",
             "name": "beni_culturali_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"beni_culturali_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Zone urbanizzate",
             "name": "zone_urbanizzate_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"zone_urbanizzate_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Acque sotterranee",
             "name": "acque_sotterranee_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"acque_sotterranee_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Acque superficiali",
             "name": "acque_superficiali_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[0,-1252344.2712499984,1252344.2712499984,6261721.356249999],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"acque_superficiali_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Aree protette",
             "name": "aree_protette_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"aree_protette_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Aree boscate",
             "name": "aree_boscate_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"aree_boscate_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Aree agricole",
             "name": "aree_agricole_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[-1252344.2712499984,5009377.085000001,1252344.2712499984,7514065.627500001],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"aree_agricole_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Addetti/utenti centri commerciali",
             "name": "centri_commerciali_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[1095801.237343751,5322463.1528125,1252344.2712499984,5479006.186718751],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"centri_commerciali_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Addetti/utenti strutture scolastiche",
             "name": "strutture_scolastiche_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"strutture_scolastiche_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Addetti/utenti strutture sanitarie",
             "name": "strutture_sanitarie_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"strutture_sanitarie_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Addetti industria e servizi",
             "name": "industria_servizi_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[-1252344.2712499984,0,2504688.5425000004,6261721.356249999],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"industria_servizi_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Popolazione turistica",
             "name": "popolazione_turistica_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"popolazione_turistica_all","styles":""}]
         },
         {
             "source": "losetiled",
             "title": "Popolazione residente",
             "name": "popolazione_residente_all",
             "group": ["Targets","Bersagli","Cibles","Betroffene Elemente"],
-            "visibility": false
+			"queryable": true,
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677],"width":256,"height":256,"format":"image/png","layers":"popolazione_residente_all","styles":""}]
         },{
             "source": "losetiled",
             "title": "Grafo stradale",
             "name": "grafo_stradale",
-            "displayInLayerSwitcher": true,
-            "tiled": true,
             "group": ["Roads","Strade","Routes"," Straßen"],
-            "visibility": false
+			"queryable": true,
+			"llbbox": [10.131616197698666,43.739871237485104,10.757333768786667,44.294290441008144],
+            "visibility": false,
+			"tileSets": [{"srs":{"EPSG:900913":true},"bbox":{"EPSG:900913":{"bbox":[939258.2034374997,5322463.1528125,1252344.2712499984,5635549.220624998],"srs":"EPSG:900913"}},"resolutions":[156543.03390625,78271.516953125,39135.7584765625,19567.87923828125,9783.939619140625,4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677,0.14929107084870338,0.07464553542435169,0.037322767712175846,0.018661383856087923,0.009330691928043961,0.004665345964021981,0.0023326729820109904,0.0011663364910054952,0.0005831682455027476,0.0002915841227513738,0.0001457920613756869],"width":256,"height":256,"format":"image/png","layers":"grafo_stradale","styles":""}]
         }
         ]
     },
