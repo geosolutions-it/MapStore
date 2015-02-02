e031810fccfe8c777d1e9e1c7853d6d1085dc593
Lorenzo Pini <lore.pini@tin.it>
2015-02-02 11:20:01 +0100

    Let Application files override the default ones

--- mapcomposer/app/applications/geocollect/config.js
+++ mapcomposer/app/applications/geocollect/config.js
@@ -1,5 +1,5 @@
 exports.config = function(urls, middleware) {
 	urls[0] = [(/^\/(index(.html)?)?/), require("./index").app];
-	urls[0] = [(/^\/(manager(.html)?)?/), require("./manager").app];
-	middleware.push(require("ringo/middleware/static").middleware({base: module.resolve("static")}));
+	urls[3] = [(/^\/(manager(.html)?)?/), require("./manager").app];
+	middleware.unshift(require("ringo/middleware/static").middleware({base: module.resolve("static")}));
 };
