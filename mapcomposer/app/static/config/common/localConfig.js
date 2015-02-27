85643308f451d6c5724295a0ff52d8a2b640dd66
Riccardo Mari <rmari76@gmail.com>
2015-02-27 15:48:56 +0100

    Fix about MAPSTORECONFIG error load

--- mapcomposer/app/static/config/common/localConfig.js
+++ mapcomposer/app/static/config/common/localConfig.js
@@ -1,7 +1,8 @@
 /** This file contains the common configuration options 
  *  that can be overridden by the serverConfig objects in templates */
 var localConfig = {
-   geoStoreBase: "http://localhost:8080/geostore/rest/",
+   geoStoreBase: "",
    proxy:"/http_proxy/proxy/?url=",
-   defaultLanguage: "it"
+   defaultLanguage: "it",
+   skipCustomConfigs: true
 };
