fb6fc860b78d7471df9f60887e3f427968a0a0b1
kappu <kappu72gmail.com>
2015-03-02 12:51:15 +0100

    Fixd bug in loading multiple describ layer request close #569

--- mapcomposer/app/static/externals/gxp/src/script/plugins/WMSSource.js
+++ mapcomposer/app/static/externals/gxp/src/script/plugins/WMSSource.js
@@ -327,6 +327,7 @@ gxp.plugins.WMSSource = Ext.extend(gxp.plugins.LayerSource, {
 		var defProp = this.getDefaultProps(original, config);            
 		
 		config = Ext.applyIf(defProp, config);
+		
 		// If the layer is not available in the map projection, find a
 		// compatible projection that equals the map projection. This helps
 		// us in dealing with the different EPSG codes for web mercator.
@@ -394,6 +395,7 @@ gxp.plugins.WMSSource = Ext.extend(gxp.plugins.LayerSource, {
 		
 		// use all params from original
 		params = Ext.applyIf(params, layer.params);
+
 		// /////////////////////////////////////////////////////////
 		// Checking if the OpenLayers transition should be 
 		// disabled (transitionEffect: null).
