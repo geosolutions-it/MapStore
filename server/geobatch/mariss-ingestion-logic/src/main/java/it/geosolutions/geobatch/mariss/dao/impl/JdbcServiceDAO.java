7ab5af985e4a76d49382f5d06cb1aaf415db8dce
afabiani <alessio.fabiani@geo-solutions.it>
2015-03-02 10:19:53 +0100

    - fix on JdbcServiceDAO

--- server/geobatch/mariss-ingestion-logic/src/main/java/it/geosolutions/geobatch/mariss/dao/impl/JdbcServiceDAO.java
+++ server/geobatch/mariss-ingestion-logic/src/main/java/it/geosolutions/geobatch/mariss/dao/impl/JdbcServiceDAO.java
@@ -66,7 +66,7 @@ public class JdbcServiceDAO implements ServiceDAO {
     public boolean insert(Service service) {
 
         boolean result = false;
-        
+
         String sql = "INSERT INTO SERVICE (\"SERVICE_ID\", \"PARENT\", \"USER\", \"STATUS\") VALUES (?, ?, ?, ?)";
         Connection conn = null;
 
@@ -91,7 +91,7 @@ public class JdbcServiceDAO implements ServiceDAO {
                 }
             }
         }
-        
+
         return result;
 
     }
@@ -100,7 +100,7 @@ public class JdbcServiceDAO implements ServiceDAO {
     public boolean insertOrUpdate(AreaOfInterest aoi) {
 
         boolean result = false;
-        
+
         String sql = "SELECT * FROM AOIS WHERE service_name = ?";
         Connection conn = null;
 
@@ -112,11 +112,11 @@ public class JdbcServiceDAO implements ServiceDAO {
             boolean performUpdate = rs.next();
             rs.close();
 
-            if (performUpdate) 
-            {
+            if (performUpdate) {
                 // perform update
                 sql = "UPDATE aois SET the_geom=?, start=?, \"end\"=?, status=?, \"desc\"=? WHERE service_name = ?";
-                if (!ps.isClosed()) ps.close();
+                if (!ps.isClosed())
+                    ps.close();
                 ps = conn.prepareStatement(sql);
                 ps.setString(1, aoi.getTheGeom());
                 ps.setDate(2, aoi.getStartTime());
@@ -125,12 +125,11 @@ public class JdbcServiceDAO implements ServiceDAO {
                 ps.setString(5, aoi.getDescription());
                 ps.setString(6, aoi.getServiceId());
                 result = ps.execute() && ps.getUpdateCount() > 0;
-            }
-            else
-            {
+            } else {
                 // perform insert
                 sql = "INSERT INTO aois(service_name, the_geom, start, \"end\", status, \"desc\") VALUES (?, ?, ?, ?, ?, ?)";
-                if (!ps.isClosed()) ps.close();
+                if (!ps.isClosed())
+                    ps.close();
                 ps = conn.prepareStatement(sql);
                 ps.setString(1, aoi.getServiceId());
                 ps.setString(2, aoi.getTheGeom());
@@ -141,7 +140,7 @@ public class JdbcServiceDAO implements ServiceDAO {
                 result = ps.executeUpdate() > 0;
             }
             ps.close();
-                        
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
 
@@ -153,9 +152,9 @@ public class JdbcServiceDAO implements ServiceDAO {
                 }
             }
         }
-        
+
         return result;
-        
+
     }
 
     @Override
@@ -164,63 +163,75 @@ public class JdbcServiceDAO implements ServiceDAO {
         String sql = "SELECT * FROM SERVICE WHERE \"SERVICE_ID\" = ?";
 
         Connection conn = null;
-
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         try {
             conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
+            ps = conn.prepareStatement(sql);
             ps.setString(1, serviceId);
             Service service = null;
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             if (rs.next()) {
                 service = new Service(rs.getString("SERVICE_ID"), rs.getString("PARENT"),
-                                      rs.getString("USER"), rs.getString("STATUS"));
+                        rs.getString("USER"), rs.getString("STATUS"));
                 service.setId(rs.getInt("ID"));
+            } else {
+                return null;
             }
-            rs.close();
-            ps.close();
-            
+
             // Retrieve the AOI
             sql = "SELECT fid, \"desc\", service_name, asewkt(the_geom) as thegeom, start, \"end\", status FROM AOIS WHERE service_name = ?";
-            
+
             ps = conn.prepareStatement(sql);
             String userId = service.getUser();
-            ps.setString(1, userId+"@"+serviceId);
+            ps.setString(1, userId + "@" + serviceId);
             AreaOfInterest aoi = null;
             rs = ps.executeQuery();
             if (rs.next()) {
-                aoi = new AreaOfInterest(serviceId,
-                                         rs.getString("desc"),
-                                         rs.getString("thegeom"), 
-                                         rs.getDate("start"), 
-                                         rs.getDate("end"), 
-                                         rs.getString("status"));
+                aoi = new AreaOfInterest(serviceId, rs.getString("desc"), rs.getString("thegeom"),
+                        rs.getDate("start"), rs.getDate("end"), rs.getString("status"));
                 aoi.setId(rs.getInt("fid"));
                 service.setAoi(aoi);
             }
             rs.close();
             ps.close();
-            
+
             // Retrieve the SENSORS
             sql = "SELECT id, sensor_type, sensor_mode, service_id FROM sensor WHERE service_id = ?";
-            
+
             ps = conn.prepareStatement(sql);
             ps.setString(1, service.getServiceId());
             List<Sensor> sensors = new ArrayList<Sensor>();
             rs = ps.executeQuery();
             while (rs.next()) {
-                Sensor sensor = new Sensor(rs.getString("sensor_type"), new SensorMode(rs.getString("sensor_mode")));
+                Sensor sensor = new Sensor(rs.getString("sensor_type"), new SensorMode(
+                        rs.getString("sensor_mode")));
                 sensor.setId(rs.getInt("id"));
                 sensors.add(sensor);
             }
             rs.close();
             ps.close();
-            
+
             service.setSensors(sensors);
-            
+
             return service;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+
             if (conn != null) {
                 try {
                     conn.close();
@@ -233,7 +244,7 @@ public class JdbcServiceDAO implements ServiceDAO {
 
     @Override
     public List<Service> findByUser(String userId) {
-    
+
         String sql = "SELECT * FROM SERVICE WHERE \"USER\" = ?";
 
         Connection conn = null;
@@ -247,56 +258,53 @@ public class JdbcServiceDAO implements ServiceDAO {
             Service service;
             while (rs.next()) {
                 service = new Service(rs.getString("SERVICE_ID"), rs.getString("PARENT"),
-                                      rs.getString("USER"), rs.getString("STATUS"));
+                        rs.getString("USER"), rs.getString("STATUS"));
                 service.setId(rs.getInt("ID"));
-                
+
                 services.add(service);
             }
             rs.close();
             ps.close();
-            
-            for (Service ss : services) 
-            {
-                String serviceId = userId+"@"+ss.getServiceId();
-                
+
+            for (Service ss : services) {
+                String serviceId = userId + "@" + ss.getServiceId();
+
                 // Retrieve the AOI
                 sql = "SELECT fid, \"desc\", service_name, asewkt(the_geom) as thegeom, start, \"end\", status FROM AOIS WHERE service_name = ?";
-                
+
                 ps = conn.prepareStatement(sql);
                 ps.setString(1, serviceId);
                 AreaOfInterest aoi = null;
                 rs = ps.executeQuery();
                 if (rs.next()) {
-                    aoi = new AreaOfInterest(serviceId,
-                                            rs.getString("desc"),
-                                            rs.getString("thegeom"), 
-                                            rs.getDate("start"), 
-                                            rs.getDate("end"), 
-                                            rs.getString("status"));
+                    aoi = new AreaOfInterest(serviceId, rs.getString("desc"),
+                            rs.getString("thegeom"), rs.getDate("start"), rs.getDate("end"),
+                            rs.getString("status"));
                     aoi.setId(rs.getInt("fid"));
                     ss.setAoi(aoi);
                 }
                 rs.close();
                 ps.close();
-                
+
                 // Retrieve the SENSORS
                 sql = "SELECT id, sensor_type, sensor_mode, service_id FROM sensor WHERE service_id = ?";
-                
+
                 ps = conn.prepareStatement(sql);
                 ps.setString(1, ss.getServiceId());
                 List<Sensor> sensors = new ArrayList<Sensor>();
                 rs = ps.executeQuery();
                 while (rs.next()) {
-                    Sensor sensor = new Sensor(rs.getString("sensor_type"), new SensorMode(rs.getString("sensor_mode")));
+                    Sensor sensor = new Sensor(rs.getString("sensor_type"), new SensorMode(
+                            rs.getString("sensor_mode")));
                     sensor.setId(rs.getInt("id"));
                     sensors.add(sensor);
                 }
                 rs.close();
                 ps.close();
-                
+
                 ss.setSensors(sensors);
             }
-            
+
             return services;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -314,7 +322,7 @@ public class JdbcServiceDAO implements ServiceDAO {
     @Override
     public boolean updateServiceStatus(Service service, String status) {
         boolean result = false;
-        
+
         String sql = "UPDATE SERVICE SET \"STATUS\" = ? WHERE \"USER\" = ? AND \"SERVICE_ID\" = ?";
 
         Connection conn = null;
@@ -343,7 +351,7 @@ public class JdbcServiceDAO implements ServiceDAO {
 
     @Override
     public List<Sensor> getSensors() {
-        
+
         String sql = "SELECT * FROM sensors";
 
         Connection conn = null;
@@ -357,12 +365,12 @@ public class JdbcServiceDAO implements ServiceDAO {
             while (rs.next()) {
                 sensor = new Sensor(rs.getString("sensor"), null);
                 sensor.setId(rs.getInt("id"));
-                
+
                 sensors.add(sensor);
             }
             rs.close();
             ps.close();
-            
+
             return sensors;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -374,7 +382,7 @@ public class JdbcServiceDAO implements ServiceDAO {
                 }
             }
         }
-        
+
     }
 
     @Override
@@ -393,12 +401,12 @@ public class JdbcServiceDAO implements ServiceDAO {
             while (rs.next()) {
                 sensorMode = new SensorMode(rs.getString("sensor_mode"));
                 sensorMode.setId(rs.getInt("id"));
-                
+
                 sensorModes.add(sensorMode);
             }
             rs.close();
             ps.close();
-            
+
             return sensorModes;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -410,14 +418,14 @@ public class JdbcServiceDAO implements ServiceDAO {
                 }
             }
         }
-        
+
     }
 
     @Override
     public boolean insertOrUpdate(String serviceId, List<Sensor> sensors) {
-        
+
         boolean result = false;
-        
+
         String sql = "DELETE FROM sensor WHERE service_id = ?";
         Connection conn = null;
 
@@ -427,11 +435,11 @@ public class JdbcServiceDAO implements ServiceDAO {
             ps.setString(1, serviceId);
             ps.executeUpdate();
             ps.close();
-            
+
             sql = "INSERT INTO sensor(sensor_type, sensor_mode, service_id) VALUES (?, ?, ?)";
             ps = conn.prepareStatement(sql);
-            
-            for(Sensor sensor : sensors) {
+
+            for (Sensor sensor : sensors) {
                 ps.setString(1, sensor.getSensor());
                 ps.setString(2, sensor.getSensorMode().getSensorMode());
                 ps.setString(3, serviceId);
@@ -450,9 +458,9 @@ public class JdbcServiceDAO implements ServiceDAO {
                 }
             }
         }
-        
+
         return result;
-        
+
     }
 
 }
