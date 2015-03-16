83ea5c7e05c56bcbedeb704b22c470543beb497c
Lorenzo Pini <lore.pini@tin.it>
2015-03-16 18:17:40 +0100

    Fix RingoJS config

--- mapcomposer/app/applications/he/config.js
+++ mapcomposer/app/applications/he/config.js
@@ -1,4 +1,5 @@
 exports.config = function(urls, middleware) {
-	urls[0] = [(/^\/(gcd(.html)?)/), require("./gcd").app];
-	middleware.unshift(require("ringo/middleware/static").middleware({base: module.resolve("static")}));
+    urls[0] = [(/^\/(index(.html)?)?/), require("../../root/loginpage").app];
+    urls.push([(/^\/(gcd(.html)?)/), require("./gcd").app]);
+    middleware.unshift(require("ringo/middleware/static").middleware({base: module.resolve("static")}));
 };
