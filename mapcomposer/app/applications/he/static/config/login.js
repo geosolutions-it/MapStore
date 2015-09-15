8b7173ec3b5abcf676af7c6f33bf74b2e7d7f1ab
kappu <kappu72gmail.com>
2015-09-15 18:59:29 +0200

    Added dinamic year to footer

--- mapcomposer/app/applications/he/static/config/login.js
+++ mapcomposer/app/applications/he/static/config/login.js
@@ -25,7 +25,7 @@
    },
    
    "footer": {
-		"html": "<div id=\"footer\"><img src=\"images/banner/logo_footer.png\" > © Hart Energy 2014 &nbsp;</div>",
+		"html": "<div id=\"footer\"><img src=\"images/banner/logo_footer.png\" > © Hart Energy </div>",
 		"css": "<style type='text/css'>#footer{background-color:black;height:100%;text-align:right;color:white;line-height:30px} #footer img{text-align:left; float:left; margin:5px;}</style>",
 		"container": {
 			"border": false,
