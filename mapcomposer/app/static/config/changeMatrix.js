1a452d7d0555fc2c2f3f714d5c0afb09cc94b5d2
afabiani <alessio.fabiani@geo-solutions.it>
2016-05-20 11:36:28 +0200

    - minor improvements

--- mapcomposer/app/static/config/changeMatrix.js
+++ mapcomposer/app/static/config/changeMatrix.js
@@ -519,14 +519,14 @@
             	{"layer": "it.crisp:corine_L1", "level": 1, "values": [1,2,3,4,5]},
             	{"layer": "it.crisp:corine_L2", "level": 2, "values": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]},
             	{"layer": "it.crisp:corine_L3", "level": 3, "values": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]},
-            	{"layer": "it.crisp:touring", "level": 4, "values": [0,1,2,3,4,5,6,7,8,9,10,11,12,13]}
+            	{"layer": "it.crisp:touring", "level": 4, "values": [1,2,3,4,5,6,7,8,9,10,11,12,13]}
             ],
             "layersPixelSizes": [
-            	{"layer": "it.crisp:touring",     "pixelSize": 400},
-            	{"layer": "it.crisp:corine_L1",   "pixelSize": 400},
-            	{"layer": "it.crisp:corine_L2",   "pixelSize": 400},
-            	{"layer": "it.crisp:corine_L3",   "pixelSize": 400},
-            	{"layer": "it.crisp:urban_grids", "pixelSize": 400}
+            	{"layer": "it.crisp:touring",     "pixelSize": 100},
+            	{"layer": "it.crisp:corine_L1",   "pixelSize": 100},
+            	{"layer": "it.crisp:corine_L2",   "pixelSize": 100},
+            	{"layer": "it.crisp:corine_L3",   "pixelSize": 100},
+            	{"layer": "it.crisp:urban_grids", "pixelSize": 100}
             ],
             "splitPanels": true,
             "wfsChangeMatrisGridPanelID": "wfsChangeMatrisGridPanel_tabpanel",
