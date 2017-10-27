85c121a0f7cc85978063058d69ffa3b7343b983d
Lorenzo Pini <lore.pini@tin.it>
2017-10-27 11:33:12 +0200

    Use https when loading Google jsapi

--- mapcomposer/app/static/externals/gxp/src/script/plugins/GoogleEarth.js
+++ mapcomposer/app/static/externals/gxp/src/script/plugins/GoogleEarth.js
@@ -254,7 +254,7 @@ gxp.plugins.GoogleEarth.loader = new (Ext.extend(Ext.util.Observable, {
         };
         
         var script = document.createElement("script");
-        script.src = "http://www.google.com/jsapi?" + Ext.urlEncode(params);
+        script.src = "https://www.google.com/jsapi?" + Ext.urlEncode(params);
 
         // cancel loading if monitor is not ready within timeout
         var errback = options.errback || Ext.emptyFn;
